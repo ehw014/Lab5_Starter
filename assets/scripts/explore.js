b5074517d48df7280de80c267d0e38928bb4572c
Evan Wu <evan.h.wu@gmail.com>
2024-05-11 23:07:59 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,45 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const selectElement = document.querySelector("select");
+const speech = window.speechSynthesis;
+let voices = [];
 
 function init() {
-  // TODO
+  voices = speech.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    selectElement.appendChild(option);
+  }
 }
+
+const buttonElement = document.querySelector("button");
+const textElement = document.getElementById("text-to-speak");
+
+buttonElement.addEventListener("click", function(){
+  const phrase = new SpeechSynthesisUtterance(textElement.value);
+  const selectedVoice = selectElement.selectedOptions[0].getAttribute("data-name");
+  for(let i =0; i < voices.length; i++) {
+    if(voices[i].name === selectedVoice)
+      phrase.voice = voices[i];
+  }
+  speech.speak(phrase);
+})
+
+const smileElement = document.querySelector("img");
+
+setInterval(function(){  if (!speech.speaking)
+  smileElement.src = "../assets/images/smiling.png";
+else
+  smileElement.src = "../assets/images/smiling-open.png";
+
+},100);
